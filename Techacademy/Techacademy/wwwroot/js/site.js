49ecac0965f7c136fb912c8e4735c0f10cc09d13
ozansa <159722371+ozansa@users.noreply.github.com>
2024-09-03 22:02:02 +0100

    Final Assignment for TAVS02E1 - Visual Studio

---
+++ Techacademy/Techacademy/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+setInterval(() => {
+    document.getElementById('animated-heading').style.color =
+        `hsl(${Math.random() * 360}, 100%, 50%)`;
+}, 500);
